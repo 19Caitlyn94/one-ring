56dbd90ccede342409d696594cbe1e0ab35aae5a
caitlynfowkes <caitlynfowkes@gmail.com>
2022-02-19 23:26:38 +0200

    Add post type model

--- backend/onering/posts/admin.py
+++ backend/onering/posts/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import PostType
 
-# Register your models here.
+@admin.register(PostType)
+class PostType(admin.ModelAdmin):
+    fields = ('posttype_id', 'name', 'request_url')
+    list_display = ('name', 'posttype_id')
